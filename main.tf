84b7a0a6c27dd98f5bf6b9eef631b0928c0396e0
Leonard Villegas <leonard.villegas@sourcemeridian.com>
2025-06-16 10:40:02 -0500

    Changes to the infra

--- main.tf
+++ main.tf
@@ -1,18 +1,14 @@
-provider "aws" {
-  region = var.aws_region
-}
-
 module "vpc" {
   source     = "./modules/vpc"
-  cidr_block = "10.0.0.0/16"
-  name       = "leo_vpc"
+  cidr_block = var.vpc_cidr
+  name       = var.vpc_name
 }
 
 module "network" {
   source       = "./modules/network"
   vpc_id       = module.vpc.vpc_id
-  public_cidr  = "10.0.2.0/24"
-  private_cidr = "10.0.1.0/24"
+  public_cidr  = var.public_subnet_cidr
+  private_cidr = var.private_subnet_cidr
   az           = var.aws_sub_region
 }
 
@@ -25,9 +21,9 @@ module "ec2" {
   source            = "./modules/ec2"
   ami               = var.instance_ami
   instance_type     = var.instance_type
-  public_subnet_id = module.network.public_subnet_id
+  public_subnet_id  = module.network.public_subnet_id
   private_subnet_id = module.network.private_subnet_id
-  key_name          = "leonard-tf-key"
-  public_key_path   = "leonard-tf-key.pub"
+  key_name          = var.key_name
+  public_key_path   = var.public_key_path
   security_group_id = module.security.security_group_id
 }
--- variables.tf
+++ variables.tf
@@ -1,23 +1,59 @@
 variable "aws_region" {
-  type = string
+  type        = string
   description = "AWS Region to be deployed"
-  default = "us-east-1"
+  default     = "us-east-1"
 }
 
 variable "aws_sub_region" {
-  type = string
+  type        = string
   description = "AWS SubRegion to be deployed"
-  default = "us-east-1a"
+  default     = "us-east-1a"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_name" {
+  type        = string
+  description = "Name of the VPC"
+  default     = "leo_vpc"
+}
+
+variable "public_subnet_cidr" {
+  type        = string
+  description = "CIDR block for the public subnet"
+  default     = "10.0.2.0/24"
+}
+
+variable "private_subnet_cidr" {
+  type        = string
+  description = "CIDR block for the private subnet"
+  default     = "10.0.1.0/24"
 }
 
 variable "instance_ami" {
-  type = string
-  description = "AWS SubRegion to be deployed"
-  default = "ami-0953476d60561c955"
+  type        = string
+  description = "AMI ID for the EC2 instance"
+  default     = "ami-084568db4383264d4"
 }
 
 variable "instance_type" {
-  type = string
-  description = "AWS SubRegion to be deployed"
-  default = "t2.micro"
+  type        = string
+  description = "Instance type for the EC2 instance"
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  type        = string
+  description = "Name of the SSH key pair"
+  default     = "leonard-tf-key"
+}
+
+variable "public_key_path" {
+  type        = string
+  description = "Path to the public SSH key"
+  default     = "leonard-tf-key.pub"
 }
